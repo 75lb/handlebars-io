c6a48f9adff4548b456da96fbdc7287ce36614ee
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-05-07 15:44:23 +0200

    escape option

--- helpers/read.js
+++ helpers/read.js
@@ -1,8 +1,21 @@
 var mfs = require("more-fs");
 
+function escapeForJSON (str) {
+    return str
+      .replace(/[\"]/g, '\\"')
+      .replace(/[\\]/g, '\\\\')
+      .replace(/[\/]/g, '\\/')
+      .replace(/[\b]/g, '\\b')
+      .replace(/[\f]/g, '\\f')
+      .replace(/[\n]/g, '\\n')
+      .replace(/[\r]/g, '\\r')
+      .replace(/[\t]/g, '\\t'); 
+}
+
 module.exports = function(handlebars){
-    handlebars.registerHelper("read", function(filename){
-        console.log(filename)
-        return mfs.read(filename);
+    handlebars.registerHelper("read", function(filename, escape){
+        var result = mfs.read(filename);
+        if (escape === true) result = escapeForJSON(result);
+        return result;
     });
 };
