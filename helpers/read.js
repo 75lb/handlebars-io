f807a2b3fb33a870ea00e4760bb08a0b56c7b644
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-05-21 15:38:38 +0200

    wip

--- helpers/read.js
+++ helpers/read.js
@@ -2,9 +2,17 @@ var mfs = require("more-fs"),
     w = require("wodge");
 
 module.exports = function(handlebars){
-    handlebars.registerHelper("read", function(filename, escape){
-        var result = mfs.read(filename);
-        if (escape === true) result = w.escapeForJSON(result);
-        return result;
+    handlebars.registerHelper("read", function(filename){
+        if (Array.isArray(filename)){
+            var filenames = filename;
+            return filenames.map(function(file){
+                return {
+                    filename: file,
+                    content: mfs.read(file)
+                };
+            });
+        } else {
+            return mfs.read(filename);
+        }
     });
 };
