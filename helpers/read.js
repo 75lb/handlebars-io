c569e8b662567865026ab3436cb84bd6a5759124
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-05-23 09:43:31 +0200

    modified

--- helpers/read.js
+++ helpers/read.js
@@ -1,14 +1,17 @@
 var mfs = require("more-fs"),
-    w = require("wodge");
+    w = require("wodge"),
+    fs = require("fs");
 
 module.exports = function(handlebars){
     handlebars.registerHelper("read", function(filename){
         if (Array.isArray(filename)){
             var filenames = filename;
             return filenames.map(function(file){
+                var stat = fs.statSync(file);
                 return {
                     filename: file,
-                    content: mfs.read(file)
+                    content: mfs.read(file),
+                    modified: stat.mtime
                 };
             });
         } else {
